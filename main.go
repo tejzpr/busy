ba169c9260c024c097c452c384d90ae5a2177c99
Tejus <tejzpr@gmail.com>
2021-02-24 17:43:04 -0800

    Updates for windows

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type caller func()
 func main() {
 	noui := flag.Bool("noui", false, "Disable busy UI")
 	flag.Parse()
+	moveChan := make(chan bool)
 	start := time.Now()
 	robotgo.EventHook(hook.KeyDown, []string{"q", "ctrl", "shift"}, func(e hook.Event) {
 		fmt.Println("Ran for :", time.Since(start))
@@ -33,19 +34,30 @@ func main() {
 	}()
 
 	fmt.Println("Press ctrl+shift+q to exit")
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(1 * time.Second)
 
 	runner := func(fn caller) {
 		for {
 			select {
 			case <-ticker.C:
 				fn()
-				robotgo.MoveMouseSmooth(rand.Intn(1000), rand.Intn(1000), 1.0, 1.0)
-				robotgo.MoveMouseSmooth(rand.Intn(1000), rand.Intn(1000), 1.0, 1.0)
-				robotgo.KeyTap("shift")
+				// Start this up as a go routine soo that it does not block the timer update
+				// Blocking channel takes in a single instruction at a time
+				go func() {
+					moveChan <- true
+				}()
 			}
 		}
 	}
+
+	go func() {
+		for range moveChan {
+			randomX := rand.Intn(1000)
+			randomY := rand.Intn(1000)
+			robotgo.MoveMouseSmooth(randomX, randomY, 1.0, 1.0)
+			robotgo.KeyTap("shift")
+		}
+	}()
 
 	if !*noui {
 		a := app.New()
@@ -64,7 +76,7 @@ func main() {
 		go runner(func() {
 			w.RequestFocus()
 			w.CenterOnScreen()
-			busy.Text = fmt.Sprintf("I've been busy for : %s", time.Since(start).Round(5*time.Second).String())
+			busy.Text = fmt.Sprintf("I've been busy for : %s", time.Since(start).Round(1*time.Second).String())
 			busyContainer.Refresh()
 		})
 		w.ShowAndRun()
